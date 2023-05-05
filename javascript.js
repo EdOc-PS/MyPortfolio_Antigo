cc1783b54e196f2f2c74e3c1cd132da0511c60a9
Eduardo Octávio <94386792+EdOc-PS@users.noreply.github.com>
2023-05-05 14:54:06 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,13 +1,49 @@
 var buttonmenu = document.querySelector(".buttonmenu")
 var divprin = document.querySelector(".principal")
-var divhidden1= document.querySelector(".escape")
-function some ()
+var divprin2 = document.querySelector(".principal2")
+var divprin3 = document.querySelector(".principal3")
+
+function somemim()
+{
+    if(divprin.style.display === "none") {
+        divprin.style.display = "inline-flex";
+        divprin2.style.display = "none";
+        divprin3.style.display = "none";
+    } else {
+      divprin.style.display = "inline-flex";
+      divprin2.style.display = "none";
+      divprin3.style.display = "none";
+    
+  }
+}
+
+function somecar()
 {
     
-    if(divprin.style.display === "none") {
-        divprin.style.display = "block";
+    if(divprin2.style.display === "none") {
+        divprin2.style.display = "inline-flex";
+        divprin.style.display = "none";
+        divprin3.style.display = "none";
+     
     } else {
       divprin.style.display = "none";
-      divhidden1.style.display ="block";
+      divprin3.style.display = "none";
+      divprin2.style.display = "inline-flex";
+  }
+}
+
+function someexp()
+{
+    if(divprin3.style.display === "none") {
+      divprin3.style.display = "inline-flex";
+      divprin.style.display = "none";
+      divprin2.style.display = "none";
+      
+    } else {
+      divprin.style.display = "none";
+      divprin2.style.display = "none";
+      divprin3.style.display = "inline-flex";
+
+  
   }
 }
